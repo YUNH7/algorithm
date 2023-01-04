f7aea4fbdb427d2e5bad72615ce526fed700e152
YUNH7 <113083398+YUNH7@users.noreply.github.com>
2023-01-04 20:33:29 +0900

    [level 0] Title: 가위 바위 보, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,9 @@
+function solution(rsp) {
+    let result =''
+    for (let i = 0; i< rsp.length; i++) {
+        if (rsp[i] === '2') result += '0'
+        else if (rsp[i] === '0') result += '5'
+        else result +='2'
+    }
+    return result
+}
