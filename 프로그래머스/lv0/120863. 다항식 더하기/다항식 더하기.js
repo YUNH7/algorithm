f0c7496f3b1465b7f89d1c4f855818ac339a3edd
YUNH7 <113083398+YUNH7@users.noreply.github.com>
2023-01-07 12:25:15 +0900

    [level 0] Title: 다항식 더하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -2,9 +2,8 @@ function solution(polynomial) {
     // x와 상수로 나누기
     let xNum = 0
     let num = 0
-    for (let el of polynomial.split(' ')) {
-        if (el === '+') continue
-        else if (el[el.length - 1] === 'x') {
+    for (let el of polynomial.split(' + ')) {
+        if (el[el.length - 1] === 'x') {
             xNum += parseInt(el)? parseInt(el) : 1
         } else {
             num += Number(el)
