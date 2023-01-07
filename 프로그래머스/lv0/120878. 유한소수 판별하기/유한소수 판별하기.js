760f8908e91531d58a16ef55eb27160a11eab52d
YUNH7 <113083398+YUNH7@users.noreply.github.com>
2023-01-07 16:31:12 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -7,34 +7,14 @@ function euc(a, b) {
     return euc(min, rest)
 }
 
-function prime(n) { // n이 소수인지 구하기
-    for (let i = 2; i < n; i++) {
-        if (n % i === 0) {
-            return false
-        } 
-    }
-    return true
-}
-
 function solution(a, b) {  
     let bDiv = b/euc(a,b)
     // b/div -> 소인수가 2, 5만 있으면 유한소수
     // b/div === 1이면 유한소수
     // 아니면 무한소수
     
-    let tmp = []
-    for (let i = 2; i <= bDiv; i++) {
-        if (bDiv % i === 0) {// 약수 구하기
-        tmp.push(i)          
-        }
-    }
-    
-    let result = 1
-    tmp.forEach((el) => {
-        if(prime(el) && (el !== 2 && el !== 5)){
-            result = 2        
-        }
-    })
-    return result
+    while (bDiv % 2 === 0) bDiv /= 2
+    while (bDiv % 5 === 0) bDiv /= 5
+    return bDiv === 1 ? 1 : 2
     // 유한소수 1, 무한소수 2 리턴
 }
