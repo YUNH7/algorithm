e22aa1f8268f99ed055332af7afbc6215d34d397
YUNH7 <113083398+YUNH7@users.noreply.github.com>
2023-01-07 11:49:57 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,11 +1,12 @@
 function solution(absolutes, signs) {
-    let result = absolutes.reduce((acc, cur) => acc + cur)
+//     let result = absolutes.reduce((acc, cur) => acc + cur)
     
-    for (let i = 0; i < signs.length; i++) {
-        if (!signs[i]) {
-            result -= (absolutes[i]*2)
-        }
-    }
+//     for (let i = 0; i < signs.length; i++) {
+//         if (!signs[i]) {
+//             result -= (absolutes[i]*2)
+//         }
+//     }
     
-    return result
+//     return result
+        return absolutes.reduce((acc, cur, i) => acc + (signs[i]? cur : -cur) , 0)
 }
