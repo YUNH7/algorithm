bde73782cc6d8ae9a5d1462d979fd44ce95feb2a
YUNH7 <113083398+YUNH7@users.noreply.github.com>
2023-01-11 19:58:01 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
@@ -1,12 +1,5 @@
-function fac(num) {
-    let result = BigInt(1)
-    for (let i = 1; i<=num; i++) {
-        result *= BigInt(i)
-    }
-    return result
-}
+const 팩토리얼 = (num) => num === 0 ? 1 : num * 팩토리얼(num - 1)
 
 function solution(balls, share) {
-    return fac(balls)/fac(balls-share)/fac(share)
-    
+  return Math.round(팩토리얼(balls) / 팩토리얼(balls - share) / 팩토리얼(share))
 }
