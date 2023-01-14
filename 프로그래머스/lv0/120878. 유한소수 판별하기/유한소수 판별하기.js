b61b60e0d8333210c15c86f78f00041da1fa649c
YUNH7 <113083398+YUNH7@users.noreply.github.com>
2023-01-14 11:50:03 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,20 +1,21 @@
-function euc(a, b) {
-    let min = Math.min(a, b)
-    let max = Math.max(a, b)
-    
-    let rest = max % min
-    if (rest === 0) return min
-    return euc(min, rest)
-}
+// function euc(a, b) {
+//     let rest = a % b
+//     if (rest === 0) return b
+//     return euc(b, rest)
+// }
 
-function solution(a, b) {  
-    let bDiv = b/euc(a,b)
-    // b/div -> 소인수가 2, 5만 있으면 유한소수
-    // b/div === 1이면 유한소수
-    // 아니면 무한소수
+// function solution(a, b) { 
+//     // 약분
+//     let bDiv = b/euc(a,b)
+//     // b/div -> 소인수가 2, 5만 있으면 유한소수
+//     // b/div === 1이면 유한소수
+//     // 아니면 무한소수
     
-    while (bDiv % 2 === 0) bDiv /= 2
-    while (bDiv % 5 === 0) bDiv /= 5
-    return bDiv === 1 ? 1 : 2
-    // 유한소수 1, 무한소수 2 리턴
+//     while (bDiv % 2 === 0) bDiv /= 2
+//     while (bDiv % 5 === 0) bDiv /= 5
+//     return bDiv === 1 ? 1 : 2
+//     // 유한소수 1, 무한소수 2 리턴
+// }
+function solution(a, b) {
+    return Number((a/b).toFixed(10)) == a/b ? 1 : 2
 }
