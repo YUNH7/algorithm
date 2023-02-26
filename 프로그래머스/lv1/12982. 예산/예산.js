9dfb4bb5ce22876431ddb6437fd00163f5f11bfa
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2023-02-26 22:45:41 +0900

    [level 1] Title: 예산, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,15 +1,15 @@
 function solution(d, budget) { // 신청지원액 배열, 예산
     d.sort((a,b) => a-b)
-    for (let i = 0; i < d.length; i++) {
-        budget -= d[i]
-        if (budget < 0) {
-            return i
-        }    
-    }
-    return d.length
+    // for (let i = 0; i < d.length; i++) {
+    //     budget -= d[i]
+    //     if (budget < 0) {
+    //         return i
+    //     }    
+    // }
+    // return d.length
     
-    // return d.reduce((acc, cur) => {
-    //     budget -= cur
-    //     return budget >= 0 ? acc + 1 : acc
-    // }, 0)
+    return d.reduce((acc, cur) => {
+        budget -= cur
+        return budget >= 0 ? acc + 1 : acc
+    }, 0)
 }
