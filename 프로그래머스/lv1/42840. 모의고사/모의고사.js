62971cefe1e840b6d557aad5640977415e1e857c
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2023-04-12 21:47:57 +0900

    [level 1] Title: 모의고사, Time: 3.28 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -0,0 +1,14 @@
+function solution(answers) {
+    const one = [1, 2, 3, 4, 5]
+    const two = [2, 1, 2, 3, 2, 4, 2, 5]
+    const three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
+    const result = [0, 0, 0]
+    for (let i = 0; i < answers.length; i++) {
+        if (answers[i] === one[i%one.length]) result[0]++
+        if (answers[i] === two[i%two.length]) result[1]++
+        if (answers[i] === three[i%three.length]) result[2]++
+    }
+    const max = result.reduce((a,c) => a > c ? a : c, 0)
+    return result.map((el,i) => el === max ? i+1 : 0).filter(el => el !== 0)
+}
