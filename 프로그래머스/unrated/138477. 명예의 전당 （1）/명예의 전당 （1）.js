cd4fa929e5b7a6225b8232ffce8f03089dc3d132
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2023-04-17 12:03:12 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,17 +1,28 @@
 function solution(k, score) {
-    const honor = new Array(k).fill(-1)
-    const result = []
+//     const honor = new Array(k).fill(-1)
+//     const result = []
     
-    while(score.length) {
-        const point = score.shift()
-        if (honor[0] < point) {
-            honor.splice(0,1,point)
-        }
+//     while(score.length) {
+//         const point = score.shift()
+//         if (honor[0] < point) {
+//             honor.splice(0,1,point)
+//         }
         
-        honor.sort((a,b) => a-b)
-        const min = honor.reduce((a,c) => c < a && c !== -1 ? c : a, honor[honor.length -1])
-        result.push(min)
-    }
+//         honor.sort((a,b) => a-b)
+//         const min = honor.reduce((a,c) => c < a && c !== -1 ? c : a, honor[honor.length -1])
+//         result.push(min)
+//     }
     
-    return result
+//     return result
+    
+    const stack = []
+    return score.reduce((a,c) => {
+        stack.push(c)
+        stack.sort((a,b) => a - b)
+        if(stack.length > k) {
+            stack.shift()
+        }
+        a.push(stack[0])
+        return a
+    },[])
 }
