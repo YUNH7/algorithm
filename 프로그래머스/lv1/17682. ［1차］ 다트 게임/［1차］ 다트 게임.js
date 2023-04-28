91313b04a5ea72031370b890fab2690fbd56217a
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2023-04-28 11:13:16 +0900

    [level 1] Title: [1차] 다트 게임, Time: 4.15 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -1,19 +1,19 @@
-function solution(dartResult) { // 점수|보너스SDT|[옵션*#] :string
+function solution(dartResult) { // 점수|보너스SDT|[옵션*#] :string // 1S2D*3T
     const square = {S: 1, D: 2, T: 3} 
 
-    return dartResult.match(/\d+\D+/g).reduce((a, turn, idx, arr) =>{
+    return dartResult.match(/\d+\D+/g).reduce((a, turn, idx, arr) =>{ // [ 1S, 2D*, 3T ]
         let score = parseInt(turn)
         const [bonus, opt] = turn.match(/\D/g)
         score **= square[bonus]
         
+        console.log(opt)
+        
         if (arr[idx+1]?.includes('*')) score *= 2
         
-        if (opt) {
-            if (opt === '*') {
-                score *= 2
-            } else {
-                score *= -1
-            }
+        if (opt === '*') {
+            score *= 2
+        } else if (opt === "#") {
+            score *= -1
         }
         
         return a + score
