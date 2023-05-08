c88a2eb969e4ff002484903c5d84a6a3521e8361
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2023-05-08 12:10:15 +0900

    [unrated] Title: 옹알이 (2), Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,5 +1,6 @@
 function solution(babbling) {
     return babbling
-        .map(b => b.replace(/aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma)/g, " ").replaceAll(" ",""))
-        .filter(el => el === "").length
+        .map(b => /^(aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma))+$/.test(b))
+        // .map(b => b.replace(/aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma)/g, " ").replaceAll(" ",""))
+        .filter(el => el).length
 }
