35c3e9fa18274a802e90d83d680b1497ed9bc26e
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2023-05-21 18:20:59 +0900

    [unrated] Title: 둘만의 암호, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -1,16 +1,10 @@
 function solution(s, skip, idx) { 
     // s에 해당하는 알파벳은 idx만큼 뒤의 알파벳으로 바꾸기
     // skip은 idx만큼 넘길 때 세지 않기
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    const findAlphabet = (c) => {
-        if (!skip.includes(c)) return c
-        return findAlphabet(alphabet[alphabet.indexOf(c) - 1])
-    }
-    
-    let excludeSkip = [...alphabet].filter(al => !skip.includes(al))
+    let excludeSkip = [...'abcdefghijklmnopqrstuvwxyz'].filter(al => !skip.includes(al))
     const len = excludeSkip.length
     
     return [...s]
-        .map(c => excludeSkip[(excludeSkip.indexOf(findAlphabet(c)) + idx) % len])
+        .map(c => excludeSkip[(excludeSkip.indexOf(c) + idx) % len])
         .join('')
 }
