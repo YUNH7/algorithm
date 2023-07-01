a5b199b0f85e8971d629c8e0651bc50d6973ad26
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2023-07-01 16:16:58 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,26 +1,5 @@
-function solution(n) {
-    const binaryN = n.toString(2);
-    const hasOne = binaryN.match(/1/g).length;
-    let returnN = binaryN;
-    
-    const plus1Bin = (binary) => {
-        let result = binary.slice().split('');
-        let trigger = true 
-        for (let i = binary.length - 1; i >= 0; i-- ) {
-            if (binary[i] === '0' && trigger) {
-                result[i] = '1';
-                trigger = false;
-            } else if(binary[i] === '1' && trigger) {
-                result[i] = '0';
-            }
-        }
-        
-        return trigger ? '1' + result.join('') : result.join('');
-    }
-    
-    returnN = plus1Bin(returnN)
-    while(returnN.match(/1/g).length !== hasOne) {
-        returnN = plus1Bin(returnN)
-    }
-    return parseInt(returnN,2)
+function solution(n, result=n+1) {
+    return n.toString(2).match(/1/g).length == result.toString(2).match(/1/g).length 
+        ? result 
+        : solution(n,result+1);
 }
