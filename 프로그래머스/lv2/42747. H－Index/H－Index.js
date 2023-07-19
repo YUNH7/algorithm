cbcdcb4031441d0f7aeafce955e53cb676822e35
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2023-07-19 23:42:47 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,10 +1,4 @@
 function solution(citations) {
-    const hArray = [0]
-    const max = Math.max(...citations)
-    for (let h = 1; h < max; h++) {
-        const overH = citations.filter(c => c >= h).length
-        if (overH >= h) hArray.push(h)
-        else break
-    }
-    return Math.max(...hArray)
+    return citations.sort((a, b) => b - a)
+        .reduce((a, c, idx) => c >= idx+1 ? idx+1 : a, 0)
 }
