ba293de04a4215bb1cb4eef6ce1df55cfaf2e3eb
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2023-07-20 16:35:26 +0900

    [level 2] Title: [1차] 캐시, Time: 0.41 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -6,14 +6,15 @@ function solution(cacheSize, cities) {
     for (let i = 0; i < cities.length; i++) {
         const formatCity = cities[i].toLowerCase()        
         const idx = queue.indexOf(formatCity)
+        
         if (idx === -1) {
             result += 5
+            if (queue.length >= cacheSize) queue.shift()
         } else {
             result++
             queue.splice(idx,1)
-        }
+        }    
         queue.push(formatCity)
-        if (queue.length > cacheSize) queue.shift()
     }
     
     return result
