49a75eb91466aff98b3c23d908edc59d7880ca6a
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2023-08-06 21:46:03 +0900

    [level 2] Title: 전화번호 목록, Time: 96.69 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.js
@@ -3,7 +3,7 @@ function solution(phone_book) {
     for (let i = 0; i < book.length - 1; i++) {
         const number = book[i]
         const next = book[i+1]
-        if (next.slice(0, number.length) === number) return false
+        if (next.startsWith(number)) return false
     }
     return true
 }
