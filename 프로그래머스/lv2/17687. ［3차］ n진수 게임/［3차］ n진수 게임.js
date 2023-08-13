cb37c828189e22f7be492464c72997d7dd39b961
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2023-08-13 20:43:39 +0900

    [level 2] Title: [3차] n진수 게임, Time: 1.11 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -1,26 +1,10 @@
 function solution(n, t, m, p) {
     let result = ''
-    let stack = ''
     let order = 0
-    while (result.length < t) {
-        stack += order.toString(n)
+    while (result.length < t*m) {
+        result += order.toString(n)
         order++
-        if (stack.length >= m) {
-            const rest = stack.length % m
-            if (rest) {
-                result += findNum(stack.slice(0,-rest))
-                stack = stack.slice(-rest)
-            } else {
-                result += findNum(stack.slice())
-                stack = ''
-            }
-        }
     }
-    
-    function findNum (numStr) {
-        const regex = new RegExp(`.{1,${m}}`, 'g');
-        return numStr.toUpperCase().replace(regex, (w) => w[p-1])
-    }
-    
-    return result
+    const regex = new RegExp(`.{1,${m}}`, 'g');
+    return result.toUpperCase().replace(regex, (w) => w[p-1]).slice(0,t)
 }
