dfbe569f8e1a3af525132876f92ed82b018642c3
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2024-04-28 16:40:29 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 12.59 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,3 +1,3 @@
 function solution(arr, queries) {
-    return queries.map(([s, e, k], idx) => arr.filter((el, i) => i >= s && i <= e && el > k).sort((a, b) => a - b)[0] || -1)
+    return queries.map(([s, e, k]) => arr.filter((el, i) => i >= s && i <= e && el > k).sort((a, b) => a - b)[0] || -1)
 }
