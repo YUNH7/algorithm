e1a9d61a368a94371a04f04716c2fb18b0aeab09
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2024-05-03 20:57:03 +0900

    [level 2] Title: 가장 큰 수, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,25 @@
+function solution(numbers) {
+    const firstSort = numbers.reduce((acc, num) => {
+        const cur = `${num}`;
+        const firstChar = cur[0];
+        if (!acc[firstChar]) acc[firstChar] = [];
+        acc[firstChar].push(cur);
+        return acc;
+    }, {});
+    
+    const secondSort = Object.entries(firstSort).map(([num, arr]) => {
+        if (arr.length > 1) {
+            arr.sort((a,b) => {
+                const valA = a + b;
+                const valB = b + a;
+                return +valA - +valB > 0 ? -1 : 1;
+            })
+        }
+        return [+num, arr]
+    })
+    
+    secondSort.sort((a, b) => b[0] - a[0]);
+    
+    if (secondSort.length === 1 && secondSort[0][0] === 0) return '0';
+    return secondSort.reduce((acc, [_, sortedArr]) => acc + sortedArr.join(''), '');
+}
