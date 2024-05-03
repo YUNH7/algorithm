befd6990a4adc45a299a687a098f869e904dedfb
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2024-05-03 17:17:24 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.04 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    const row = arr.length;
+    const column = arr[0].length;
+    return row === column 
+        ? arr 
+        : row > column 
+        ? arr.map(el => el.concat(new Array(row - column).fill(0)))
+        : arr.concat(new Array(column - row).fill(new Array(column).fill(0)));
+    
+    
+}
