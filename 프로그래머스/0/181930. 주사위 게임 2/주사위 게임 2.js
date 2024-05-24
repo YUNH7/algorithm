52edc2d47269f1061c26c294c60b6f952e049e01
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2024-05-24 23:35:30 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,7 +1,5 @@
-function solution(a, b, c) {
-    const same = [a === b, a === c, b === c].filter(el => el).length;
-    const result = a + b + c;
-    const result2 = result * (a**2 + b**2 + c**2);
-    const result3 = result2 * (a**3 + b**3 + c**3);
-    return same === 0 ? result : same === 1 ? result2 : result3;
+function solution(...nums) {
+    const dif = new Set(nums).size;
+    return new Array(dif === 3 ? 1 : dif === 2 ? 2 : 3).fill(0)
+        .reduce((a, c, i) => a * nums.reduce((a, c) => a + c**(i+1), 0), 1);
 }
