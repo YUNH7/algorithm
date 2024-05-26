73e6883873b26b3fa97122489a2b11282e4e6d15
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2024-05-26 16:28:28 +0900

    [level 3] Title: 야근 지수, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -0,0 +1,19 @@
+function solution(n, works) {
+    if (n > works.reduce((a,c) => a+c, 0)) return 0;
+    
+    const worksMap = works.reduce((a, c) => a.set(c, a.get(c)+1 || 1), new Map());
+    while (n > 0 && worksMap.size) {
+        const cur = Math.max(...worksMap.keys());
+        const curVal = worksMap.get(cur);
+        const dif = n - curVal;
+
+        if (dif >= 0) worksMap.delete(cur);
+        else worksMap.set(cur, -dif); 
+        
+        const prev = cur-1;
+        if (prev > 0) worksMap.set(prev, (worksMap.get(prev) || 0) + (dif >= 0 ? curVal : n));
+        n -= curVal;
+    }
+    
+    return [...worksMap.entries()].reduce((a, [val, repeat]) => a + val ** 2 * repeat, 0)
+}
