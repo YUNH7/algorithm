353241a4bcf5a940105dbfd9774dff8dfcc9cf4d
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2024-06-08 17:37:36 +0900

    [level 0] Title: 배열 만들기 5, Time: 2.00 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
@@ -1,3 +1,3 @@
 function solution(intStrs, k, s, l) {
-    return intStrs.map(intStr => +intStr.slice(s, s+l)).filter(n => n > k);
+    return intStrs.map(str => +str.slice(s, s+l)).filter(n => n > k);
 }
