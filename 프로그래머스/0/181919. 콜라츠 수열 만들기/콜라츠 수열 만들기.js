1c036f57a2aa99cac0abdf02dfde04c75142231c
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2024-06-08 17:37:09 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,9 +1,8 @@
-function solution(n) {
-    const result = [n];
-    while (result.at(-1) !== 1) {
-        const cur = result.at(-1);
-        const next = cur % 2 ? 3 * cur + 1 : cur / 2;
-        result.push(next);
-    }
-    return result;
+function solution(n) {     
+    return (function aux (num, arr) {
+        if (num === 1) return arr;
+        
+        const calcNum = num % 2 ? 3 * num + 1 : num / 2;
+        return aux(calcNum, [...arr, calcNum]);
+    }(n, [n]));
 }
