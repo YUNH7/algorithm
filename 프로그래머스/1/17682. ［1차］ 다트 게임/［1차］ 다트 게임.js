7c9c711fef72d6607fdc0537a422ec4b6831d038
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2024-06-15 23:24:29 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -0,0 +1,16 @@
+function solution(dartResult) {
+    const calcScore = {
+        S: n => n,
+        D: n => n ** 2,
+        T: n => n ** 3,
+        '*': n => n * 2,
+        '#': n => n * -1,
+        undefined: n => n
+    }
+    return dartResult.match(/\d+\D[\*#]*/g).reduce((a, c, i, arr) => {
+        const [score, area, award] = c.match(/\d+|\D|\*|#/g);
+        const nextAward = /\*/g.test(arr[i+1]) ? '*' : undefined;
+        const curScore = calcScore[nextAward](calcScore[award](calcScore[area](+score)));
+        return a + curScore;
+    }, 0)
+}
