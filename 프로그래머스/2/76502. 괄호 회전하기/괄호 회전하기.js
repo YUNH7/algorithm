0f4fb041357db4ccc76bedca021509d8794345f4
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2024-10-25 06:22:04 -0700

    [level 2] Title: 괄호 회전하기, Time: 0.99 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,12 +1,13 @@
 function solution(s) {
     const bracket = { '[': ']', '{': '}', '(': ')' };
+    const open = Object.keys(bracket);
     for (let i = 0; i < s.length; i++) {
         const str = s.slice(i) + s.slice(0, i);
         const stack = [];
         let result = 0;
         for (let j = 0; j < str.length; j++) {
             const now = str[j];
-            if (now === '[' || now === '{' || now === '(') stack.push(now);
+            if (open.includes(now)) stack.push(now);
             else if (now === bracket[stack.pop()]) {
                 if (stack.length === 0) result += 1;
             } else {
