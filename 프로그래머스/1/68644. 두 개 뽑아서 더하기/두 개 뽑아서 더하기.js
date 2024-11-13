6dd6d176525e5245b1b08408bfa74581a11c90f0
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2024-11-13 04:40:35 -0800

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.44 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+    const set = new Set();
+    
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i+1; j < numbers.length; j++)
+            set.add(numbers[i] + numbers[j]);
+    }
+    
+    return [...set].sort((a, b) => a - b);
+}
