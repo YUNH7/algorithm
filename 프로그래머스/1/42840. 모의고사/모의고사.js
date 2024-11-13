a285265dbf17ec71cb65e70a0110bb157621635c
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2024-11-13 04:55:22 -0800

    [level 1] Title: 모의고사, Time: 2.50 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,16 +1,13 @@
 function solution(answers) {
-    const students = [
-        [1, 2, 3, 4, 5],
-        [2, 1, 2, 3, 2, 4, 2, 5],
-        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    ];
-    return answers
-        .reduce((a, key, i) => students
-            .map(answers => answers[i % answers.length])
-                .map((answer, idx) => a[idx] + +(answer === key))
-        , [0, 0, 0])
-        .reduce((acc, score, i, arr) => {
-            if (Math.max(...arr) === score) acc.push(i+1)
-            return acc;
-        }, []);
+    const first = [1, 2, 3, 4, 5];
+    const second = [2, 1, 2, 3, 2, 4, 2, 5];
+    const third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    const result = [first, second, third].map(arr => {
+        const len = arr.length;
+        return answers.filter((a, i) => a === arr[i % len]).length;
+    });
+    return result.reduce((a, c, i) => {
+        if (c === Math.max(...result)) a.push(i+1);
+        return a;
+    }, [])
 }
