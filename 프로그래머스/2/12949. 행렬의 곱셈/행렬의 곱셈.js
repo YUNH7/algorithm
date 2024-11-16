66a30b552e21f4cd7f354637945d69397ec8a706
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2024-11-15 20:09:07 -0800

    [level 2] Title: 행렬의 곱셈, Time: 12.42 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,7 +1,3 @@
 function solution(arr1, arr2) {
-    return arr1.map((_, i) => arr2[0].map((_, j) => {
-        let sum = 0;
-        for (let idx = 0; idx < arr1[0].length; idx++) sum += arr1[i][idx]*arr2[idx][j];
-        return sum;
-    }));
+    return arr1.map((_, i) => arr2[0].map((_, j) => arr1[0].reduce((a, c, idx) => a + arr1[i][idx]*arr2[idx][j], 0)));
 }
