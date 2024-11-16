8f8f07536d2202650fca04d3634227eafc3b01b0
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2024-11-15 19:47:21 -0800

    [level 1] Title: 실패율, Time: 27.47 ms, Memory: 39.8 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,12 +1,11 @@
 function solution(N, stages) {
-    const blocked = stages.reduce((a, c) => {
-            a[c-1]++;
-            return a;
-        }, new Array(N+1).fill(0));
-    const players = blocked.map((b, i, arr) => ({rate: b === 0 ? 0 : b / arr.slice(i).reduce((a, c) => a+c), i}));
+    const blocked = new Array(N+1).fill(0);
+    stages.forEach(stage => blocked[stage-1]++);
+    
+    const players = blocked.map((b, i, arr) => ({rate: b === 0 ? 0 : b / arr.slice(i).reduce((a, c) => a+c), stage: i+1}));
     players.pop();
     
     return players
         .sort((a, b) => b.rate - a.rate)
-        .map(el => el.i + 1);
+        .map(el => el.stage);
 }
