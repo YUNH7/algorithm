f48f0c3d0597d732d51551ff74b507a8e8def566
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2024-12-02 06:11:57 -0800

    [level 1] Title: 신고 결과 받기, Time: 187.15 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,17 +1,17 @@
 function solution(id_list, report, k) {
-    const reportedId = report.reduce((a, c) => {
+    const reportedId = [...new Set(report)].reduce((a, c) => {
         const [reporter, villain] = c.split(' ');
-        if(!a[villain]) a[villain] = new Set();
-        a[villain].add(reporter);
+        if(!a[villain]) a[villain] = [];
+        a[villain].push(reporter);
         return a;
     }, {});
     
-    const reporters = Object.values(reportedId).filter(set => set.size >= k).map(set => [...set]).flat();
+    const reporters = Object.values(reportedId).filter(arr => arr.length >= k).flat();
     
     const mailingList = reporters.reduce((a, c) => {
         a[c] = (a[c] || 0) + 1;
         return a;
     }, {});
-
+    
     return id_list.map(id => mailingList[id] || 0);
 }
