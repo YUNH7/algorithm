148b892a70e817a42f6070e7071a8e4f6e11bb4c
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2025-01-05 12:11:57 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.02 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,21 +1,21 @@
 function solution(n, wires) {
-    const tree = new Array(n+1).fill(0).map(() => []);
+    const neighbor = new Array(n+1).fill(0).map(() => []);
     wires.forEach(([p, c]) => {
-        tree[p].push(c);
-        tree[c].push(p);        
+        neighbor[p].push(c);
+        neighbor[c].push(p);        
     });
 
-    const result = {};
+    const tree = {};
     const size = new Set();
     const aux = (node, p) => {
-        if (result[node]) return [];
-        result[node] = new Set(tree[node]);
-        result[node].add(node)
-        for (let i = 0; i < tree[node].length; i++) {
-            aux(tree[node][i], true).forEach(val => result[node].add(val));
+        if (tree[node]) return [];
+        tree[node] = new Set(neighbor[node]);
+        tree[node].add(node)
+        for (let i = 0; i < neighbor[node].length; i++) {
+            aux(neighbor[node][i], true).forEach(val => tree[node].add(val));
         }
-        size.add(result[node].size - Number(p));
-        return [...result[node]];
+        size.add(tree[node].size - Number(p));
+        return [...tree[node]];
     }
     
     aux(wires[0][0], false);
