15c10cf13f6c627547a11012c8652ee94162d859
YUNHUI JO <113083398+YUNH7@users.noreply.github.com>
2025-01-18 12:07:50 +0900

    [level 2] Title: 배달, Time: 1.23 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -5,18 +5,17 @@ function solution(N, road, K) {
             a[x][y] = h;
         }
         return a;
-    }, new Array(N+1).fill(0).map(() => new Array(N+1).fill(0)));
-    console.log(map)
-    const flag = map[1].map(el => el <= K ? el : 0);
+    }, new Array(N+1).fill(0).map(() => new Array(N+1).fill(0)));    
+    const flag = new Array(N+1).fill(0);
     
     const queue = [1];
-    while (queue.length) {
+    while (queue.length && flag.slice(1).some((el) => !el)) {
         const before = queue.shift();
         const beforeTime = flag[before];
         for (let i = 2; i < map[before].length; i++) {
             const hour = map[before][i];
             const newTime = beforeTime + hour
-            if (hour && newTime <= K && (flag[i] === 0 || flag[i] >= newTime)) {
+            if (hour && newTime <= K && (flag[i] === 0 || flag[i] > newTime)) {
                 flag[i] = newTime;
                 queue.push(i);
             } 
